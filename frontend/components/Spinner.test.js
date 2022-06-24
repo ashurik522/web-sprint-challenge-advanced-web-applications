c1989fc423fe26bf8653363146305d7955707e22
ashurik522 <ashurik522@gmail.com>
2022-06-24 15:06:24 -0400

    MVP

--- frontend/components/Spinner.test.js
+++ frontend/components/Spinner.test.js
@@ -1,5 +1,23 @@
 // Import the Spinner component into this file and test
 // that it renders what it should for the different props it can take.
-test('sanity', () => {
-  expect(true).toBe(false)
+import React from 'react'
+import { render, screen } from '@testing-library/react'
+import Spinner from './Spinner'
+import LoginForm from './LoginForm'
+import userEvent from '@testing-library/user-event'
+
+
+test('renders without error', () =>{
+  render(<Spinner on={false}/>) 
 })
+
+test('renders spinner while true, remove spinner while false', () =>{
+  const { rerender } = render(<Spinner on={true}/>) 
+  let spinnerMessage = screen.getByText(/please wait/i)
+  expect(spinnerMessage).not.toBeNull()
+
+  rerender(<Spinner on={false}/>)
+  spinnerMessage = screen.queryByText(/please wait/i)
+  expect(spinnerMessage).toBeNull()
+})
+
